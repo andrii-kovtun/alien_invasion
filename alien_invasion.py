9368c555a9cb3b6825eb1901c9a0a9385f2f5a0f
Andrii Kovtun <andri.kovtun@gmail.com>
2018-07-17 02:55:11 +0300

    added bullets fire behavior

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -17,13 +18,16 @@ def run_game():
 
     # Make a ship
     ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
 
     # Start the main loop of the program
     while True:
         # Watch for keyboard and mouse events
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
